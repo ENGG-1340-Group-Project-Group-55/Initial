9714da20643f250a22ddec949eaed51182f0e95d
lukas0621 <lukas06@naver.com>
2023-04-04 03:45:18 +0800

    Map_loader

---
+++ Map_loader.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Maps
+        {
+    private:
+            int Map_height;
+            int Map_width;
+    public:
+            Maps(int,int);
+            int map_height;
+            int map_width;
+            void Map_Loader();
+            void MapToArray();
+            void Location_accessor();
+        };
+
+int main(){
+    Maps Mp(15,15);
+    Mp.Map_Loader();
+}
+
+Maps::Maps(int a,int b){
+    Map_height = a;
+    Map_width = b;
+    map_height = Map_height;
+    map_width = Map_width;
+}
+
+void Maps::Map_Loader() {
+    ifstream inputline;
+    string nextline;
+    inputline.open("/Users/lucas/Documents/GitHub/Initial/ghost1.txt");
+    if (inputline.fail()){
+        cout<<"파일이 없다잖아 병신아!!!"<<endl;
+        exit(1);
+    }
+    while (getline(inputline,nextline)){
+        cout<<nextline<<endl;
+    }
+    inputline.close();
+}
+
+void Maps::MapToArray() {
+    char td_map [map_height][map_width];
+        for (int i=0;i<map_height;i++){
+            for (int j=0;j<map_width;j++){
+
+
+            }
+        }
+
+
+}
