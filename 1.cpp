bf3d37d43ed16dbfa748d3f163ca9387ba845f03
mck0124 <mck0124@naver.com>
2023-04-03 12:47:47 +0800

    Starting Screen
    게임 실행 및 introduction

---
+++ 1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+string support;
+
+void mainMenu() {
+    if(support == "Help" || support == "help") {
+
+        cout << "-------------------------------------------" << endl;
+        cout << "                MAIN MENU                  " << endl;
+        cout << "To quite the game, type 'quit'" << endl;
+        cout << "To continue, type 'continue'. " << endl;
+        cin >> support;
+    }
+}
+
+void gameID() {
+    string ID;
+    cout << "What is your name?" << endl;
+    cin >> ID;
+    cout << "Hello " << ID << ", Good luck on your journey" << endl;
+    cin >> support;
+}
+
+void starting() {
+    string userStart;
+    cout << "--------------------------------------------------------------------" << endl;
+    cout << "Hello, welcome to the \"Haunted Memories: The School Tragedy\"." << endl << " " << endl;
+    cout << "If you need help during the game, type 'help' to go to the main menu." << endl << " " << endl;
+    cout << "Type 'start' to start your adventure!" << endl;
+    cin >> support;
+    if (support == "Start" || support == "start") {
+
+        gameID();
+    }
+}
+
+
+int main() {
+
+    starting();
+    mainMenu();
+}
