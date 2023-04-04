7655af70a2a853757df7569f7f835e0c0115370c
kimddo1 <u3598692@connect.hku.hk>
2023-04-04 19:40:55 +0800

    minor changes
    not much

---
+++ cmake-build-debug/CMakeFiles/Map_loading.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Map_loading.dir/Map&Objects/Map_loading.cpp.o"
+  "CMakeFiles/Map_loading.dir/Map&Objects/Map_loading.cpp.o.d"
+  "Map_loading"
+  "Map_loading.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Map_loading.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/inventory2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inventory2.dir/UI/inventory2.cpp.o"
+  "CMakeFiles/inventory2.dir/UI/inventory2.cpp.o.d"
+  "inventory2"
+  "inventory2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inventory2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
