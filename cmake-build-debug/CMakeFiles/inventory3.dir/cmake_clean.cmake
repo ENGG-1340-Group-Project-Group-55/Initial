589ac19663947045604cac32e9b76e857109e90f
kimddo1 <u3598692@connect.hku.hk>
2023-04-28 03:35:33 +0800

    clean ups

---
+++ cmake-build-debug/CMakeFiles/inventory3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inventory3.dir/UI/inventory3.cpp.o"
+  "CMakeFiles/inventory3.dir/UI/inventory3.cpp.o.d"
+  "inventory3"
+  "inventory3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inventory3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
