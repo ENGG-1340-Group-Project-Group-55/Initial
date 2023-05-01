deb7c7c5b11f085966f811a38804f14b179601e2
Seungwoo0120 <u3598693@connect.hku.hk>
2023-05-01 17:56:43 +0800

    Rooms

---
+++ Random_Spawn/Diningroom.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ncurses.h>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    // Initialize ncurses
+    initscr();
+    cbreak();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+
+    // Set window size
+    int row = 30;
+    int col = 100;
+    int start_row = 0;
+    int start_col = 0;
+
+    // Load map from file
+    string filename = "Dining Room.txt";
+    ifstream file(filename);
+    vector<string> lines;
+    string line;
+    while (getline(file, line)) {
+        lines.push_back(line);
+    }
+    file.close();
+
+    // Draw map
+    for (int i = 0; i < row && i < lines.size(); i++) {
+        for (int j = 0; j < col && j < lines[i].size(); j++) {
+            mvaddch(start_row + i, start_col + j, lines[i][j]);
+        }
+    }
+
+    // Set random seed
+    srand(time(NULL));
+
+    // Spawn "*"
+    int coords[6][2] = {{14,5}, {14,34}, {17,90}, {21,30}, {21,70}, {20,5}};
+    int index = rand() % 6;
+    int x = coords[index][0];
+    int y = coords[index][1];
+    mvaddch(start_row + x, start_col + y, '*');
+
+    // Refresh screen
+    refresh();
+
+    // Wait for user input
+    getch();
+
+    // End ncurses
+    endwin();
+
+    return 0;
+}
