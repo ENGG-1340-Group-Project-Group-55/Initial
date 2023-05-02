bb6e05c2f0255ae870f5c7ddbe6aa8582a41b88e
kimddo1 <125544593+kimddo1@users.noreply.github.com>
2023-05-02 09:03:21 +0000

    .

--- Main_directory/MAIN.cpp
+++ Main_directory/MAIN.cpp
@@ -157,7 +157,7 @@ void rooms::ClubRoom()
  {
     string file_path = "/workspaces/Initial/Map_Objects/Map_resources/Rooftop.txt";
     int x_door = 49;
-    int y_door = 23;
+    int y_door = 20;
     main_engine(file_path,x_door,y_door);
  }
 
