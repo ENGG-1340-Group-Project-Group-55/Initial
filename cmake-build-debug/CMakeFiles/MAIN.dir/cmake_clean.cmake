299b708e7a983ce235e1c1a70bb15e492aea2b58
kimddo1 <u3598692@connect.hku.hk>
2023-05-05 03:03:55 +0800

    fix

---
+++ cmake-build-debug/CMakeFiles/MAIN.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MAIN.dir/CharactersDesign_Mechanism/MainEngine.cpp.o"
+  "CMakeFiles/MAIN.dir/CharactersDesign_Mechanism/MainEngine.cpp.o.d"
+  "CMakeFiles/MAIN.dir/CharactersDesign_Mechanism/doorcoordinate.cpp.o"
+  "CMakeFiles/MAIN.dir/CharactersDesign_Mechanism/doorcoordinate.cpp.o.d"
+  "CMakeFiles/MAIN.dir/Main_directory/MAIN.cpp.o"
+  "CMakeFiles/MAIN.dir/Main_directory/MAIN.cpp.o.d"
+  "CMakeFiles/MAIN.dir/Map_Objects/Map_loading.cpp.o"
+  "CMakeFiles/MAIN.dir/Map_Objects/Map_loading.cpp.o.d"
+  "MAIN"
+  "MAIN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MAIN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/MainEngine.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/M1/Documents/GitHub/Initial/CharactersDesign_Mechanism/MainEngine.cpp" "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/MainEngine.cpp.o" "gcc" "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/MainEngine.cpp.o.d"
+  "/Users/M1/Documents/GitHub/Initial/CharactersDesign_Mechanism/doorcoordinate.cpp" "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/doorcoordinate.cpp.o" "gcc" "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/doorcoordinate.cpp.o.d"
+  "/Users/M1/Documents/GitHub/Initial/Main_directory/MAIN.cpp" "CMakeFiles/MainEngine.dir/Main_directory/MAIN.cpp.o" "gcc" "CMakeFiles/MainEngine.dir/Main_directory/MAIN.cpp.o.d"
+  "/Users/M1/Documents/GitHub/Initial/Map_Objects/Map_loading.cpp" "CMakeFiles/MainEngine.dir/Map_Objects/Map_loading.cpp.o" "gcc" "CMakeFiles/MainEngine.dir/Map_Objects/Map_loading.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
