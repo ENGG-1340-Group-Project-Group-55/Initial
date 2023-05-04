d4bee3300a53754c8105b37cd82455d8967590cd
lukas0621 <117011658+lukas0621@users.noreply.github.com>
2023-05-04 12:21:26 +0000

    ver

---
+++ build/CMakeFiles/MainEngine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/MainEngine.cpp.o"
+  "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/MainEngine.cpp.o.d"
+  "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/doorcoordinate.cpp.o"
+  "CMakeFiles/MainEngine.dir/CharactersDesign_Mechanism/doorcoordinate.cpp.o.d"
+  "CMakeFiles/MainEngine.dir/Main_directory/MAIN.cpp.o"
+  "CMakeFiles/MainEngine.dir/Main_directory/MAIN.cpp.o.d"
+  "CMakeFiles/MainEngine.dir/Map_Objects/Map_loading.cpp.o"
+  "CMakeFiles/MainEngine.dir/Map_Objects/Map_loading.cpp.o.d"
+  "MainEngine"
+  "MainEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MainEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
